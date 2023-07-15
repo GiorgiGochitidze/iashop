5afc6d5727a318129a5e9acca995bd48f245e9ce
Giorgi Gochitidze <giorgigochitidze555@gmail.com>
2023-07-15 22:18:08 +0400

    new file:   src/assets/clothes.webp 	new file:   src/assets/clothes2.webp 	new file:   src/assets/clothes3.webp 	modified:   src/components/CSS/Home.css 	modified:   src/components/CSS/Products.css 	modified:   src/components/Products.jsx 	modified:   src/components/ProductsCard.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,23 @@
 import '../components/CSS/Products.css'
 import ProductsCard from './ProductsCard';
+import clothes from '../assets/clothes.webp'
+import clothes2 from '../assets/clothes2.webp'
+import clothes3 from '../assets/clothes3.webp'
 
 const Products = () => {
+
+    const first = clothes
+    const second = clothes2
+    const third = clothes3
+
     return ( 
         <section className="product-container">
-           <ProductsCard />
-           <ProductsCard />
-           <ProductsCard />
-           <ProductsCard />
+           <ProductsCard first={first} />
+           <ProductsCard first={second} />
+           <ProductsCard first={third} />
+           <ProductsCard first={second} />
+           <ProductsCard first={third} />
+           <ProductsCard first={first} />
         </section>
      );
 }
--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -1,9 +1,9 @@
 import clothes1 from '../assets/clothes1.jpg'
 
-const ProductsCard = () => {
+const ProductsCard = ({first, second, third}) => {
     return ( 
         <div className='product-card'>
-        <img src={clothes1} alt='clothes image number 1' />
+        <img src={first} alt='clothes image number 1' />
         <h4 style={{textAlign: 'center'}}>ფეხსაცმელები (ბავშვის)</h4><br />
         <p>Lorem ipsum dolor sit amet.</p><br />
         <p>10$</p>
