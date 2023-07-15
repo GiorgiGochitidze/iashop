9baa08cf0c060ee5c8db7848c2b4e5a5492662f6
Giorgi Gochitidze <giorgigochitidze555@gmail.com>
2023-07-15 15:40:49 +0400

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/Iasshop.png 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/assets/react.svg 	new file:   src/components/CSS/Home.css 	new file:   src/components/Home.jsx 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import './CSS/Home.css'
+
+const Home = () => {
+
+
+    return ( 
+        <>
+            <header>
+                <nav>
+                    <p>მთავარი</p>
+                    <p>ჩვენს შესახებ</p>
+                    <p>სიახლეები</p>
+                    <p>კონტაქტი</p>
+                </nav>
+            </header>
+        </>
+     );
+}
+ 
+export default Home;
