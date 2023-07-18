669ad6defcefdb72b01a83aa59f467c1c270c5fc
Giorgi Gochitidze <giorgigochitidze555@gmail.com>
2023-07-18 11:08:20 +0400

    modified:   src/components/CSS/Categories.css 	modified:   src/components/Categories.jsx

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -107,18 +107,12 @@ const Categories = ({ filteredCards }) => {
   ];
 
   const [categories, setCategories] = useState(initialCategories);
-  const [selectedCategory, setSelectedCategory] = useState("ყველა პროდუქტი");
+  const [selectedCategory, setSelectedCategory] = useState(null);
   const [showSubcategories, setShowSubcategories] = useState(true);
 
-  //accepting this function to check wich part of categories is active
   const handleCheckActive = (categoryName) => {
     setSelectedCategory(categoryName);
-    const updatedCategories = categories.map((category) => ({
-      ...category,
-      active: category.name === categoryName,
-    }));
-    setCategories(updatedCategories);
-
+  
     // Toggle showSubcategories state when the same category is clicked again
     if (selectedCategory === categoryName) {
       setShowSubcategories((prevShowSubcategories) => !prevShowSubcategories);
@@ -133,9 +127,9 @@ const Categories = ({ filteredCards }) => {
     }
     return filteredCards.filter((card) => card.title.includes(categoryName));
   };
-
-  return (
-    <>
+    
+    return (
+      <>
       <div className="category-container">
         <div className="category-list">
           <h1 style={{ marginTop: "20px" }}>კატეგორიები</h1>
@@ -146,42 +140,48 @@ const Categories = ({ filteredCards }) => {
               <p
                 onClick={() => handleCheckActive(category.name)}
                 style={{
-                  color: category.active ? "rgb(188, 35, 188)" : "black",
+                  color: selectedCategory === category.name ? "rgb(188, 35, 188)" : "black",
                 }}
               >
                 {category.name}
               </p>
-              {category.active &&
-                category.subcategories.length > 0 &&
-                showSubcategories && (
-                  <div style={{ marginLeft: "20px" }}>
-                    {category.subcategories.map((subcategory, subIndex) => (
-                      <p style={{ textAlign: "center" }} key={subIndex}>
-                        {subcategory}
-                      </p>
-                    ))}
-                  </div>
-                )}
+              {selectedCategory === category.name && category.subcategories.length > 0 && (
+                <div style={{ marginLeft: "20px" }}>
+                  {category.subcategories.map((subcategory, subIndex) => (
+                    <p
+                      style={{
+                        textAlign: "center",
+                        color: selectedCategory === subcategory ? "rgb(188, 35, 188)" : "black",
+                      }}
+                      key={subIndex}
+                      onClick={() => handleCheckActive(subcategory)}
+                    >
+                      {subcategory}
+                    </p>
+                  ))}
+                </div>
+              )}
             </React.Fragment>
           ))}
         </div>
 
         <div className="cards-container">
-          {filterCardsByCategory(selectedCategory).map((card) => (
-            <div
-              style={{ margin: "0 auto" }}
-              key={card.id}
-              data-aos="fade-up"
-              className="product-card"
-            >
-              <img src={card.url} alt="clothes image number 1" width="280px" height="180px" />
-              <h4 style={{ textAlign: "center" }}>{card.title}</h4>
-              <br />
-              <p>{card.description}</p>
-              <br />
-              <p>{card.price}</p>
-            </div>
-          ))}
+          {selectedCategory && (
+            filterCardsByCategory(selectedCategory).map((card) => (
+              <div
+                style={{ margin: "0 auto" }}
+                key={card.id}
+                className="product-card"
+              >
+                <img src={card.url} alt="clothes image number 1" width="280px" height="180px" />
+                <h4 style={{ textAlign: "center" }}>{card.title}</h4>
+                <br />
+                <p>{card.description}</p>
+                <br />
+                <p>{card.price}</p>
+              </div>
+            ))
+          )}
         </div>
       </div>
       <Footer />
