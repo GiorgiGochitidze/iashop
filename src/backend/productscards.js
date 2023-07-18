2a129820c047d4a30f522d798171a3523037ca43
Giorgi Gochitidze <giorgigochitidze555@gmail.com>
2023-07-18 09:56:09 +0400

    modified:   src/backend/productscards.js 	modified:   src/components/CSS/Categories.css

--- src/backend/productscards.js
+++ src/backend/productscards.js
@@ -1,6 +1,23 @@
 import clothes1 from '../assets/clothes1.jpg'
 import clothes2 from '../assets/clothes2.webp'
 import clothes3 from '../assets/clothes3.webp'
+import shoes1 from '../assets/shoes1.jpg'
+import shoes2 from '../assets/shoes2.jpg'
+import shoes3 from '../assets/shoes3.jpg'
+import shoes4 from '../assets/shoes4.jpg'
+import shoes5 from '../assets/shoes5.jpg'
+import shoes6 from '../assets/shoes6.jpg'
+import shoes7 from '../assets/shoes7.jpg'
+import shoes8 from '../assets/shoes8.jpg'
+import shoes9 from '../assets/shoes9.jpg'
+import shoes10 from '../assets/shoes10.jpg'
+import shoes11 from '../assets/shoes11.jpg'
+import shoes12 from '../assets/shoes12.jpg'
+import shoes13 from '../assets/shoes13.jpg'
+import shoes14 from '../assets/shoes14.jpg'
+import shoes15 from '../assets/shoes15.jpg'
+import shoes16 from '../assets/shoes16.jpg'
+import shoes17 from '../assets/shoes17.jpg'
 
 
 export const Cards = [
@@ -23,6 +40,142 @@ export const Cards = [
     {
         "id": 2,
         "url": `${clothes3}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 3,
+        "url": `${shoes1}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+    
+    {
+        "id": 4,
+        "url": `${shoes2}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 5,
+        "url": `${shoes3}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 6,
+        "url": `${shoes4}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 7,
+        "url": `${shoes5}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 8,
+        "url": `${shoes6}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 9,
+        "url": `${shoes7}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 10,
+        "url": `${shoes8}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 11,
+        "url": `${shoes9}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 12,
+        "url": `${shoes10}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 13,
+        "url": `${shoes11}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 14,
+        "url": `${shoes12}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 15,
+        "url": `${shoes13}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 16,
+        "url": `${shoes14}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 17,
+        "url": `${shoes15}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 18,
+        "url": `${shoes16}`,
+        "title": 'ტანსაცმელები მამაკაცი',
+        "description": "მამაკაცების ტანსაცმელი",
+        "price": "15$"
+    },
+
+    {
+        "id": 19,
+        "url": `${shoes17}`,
         "title": 'ტანსაცმელები მამაკაცი',
         "description": "მამაკაცების ტანსაცმელი",
         "price": "15$"
