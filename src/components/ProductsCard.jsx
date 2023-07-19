29859da6eb246892089a929263961faa76028ae7
Giorgi Gochitidze <giorgigochitidze555@gmail.com>
2023-07-19 13:06:54 +0400

    modified:   package-lock.json 	modified:   package.json 	modified:   src/backend/productscards.js 	modified:   src/components/CSS/Products.css 	modified:   src/components/Categories.jsx 	modified:   src/components/ProductsCard.jsx

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -2,7 +2,7 @@ const ProductsCard = ({ filteredCards }) => {
     return (
       <>
         {filteredCards.map((arrays) => (
-          <div style={{margin: '20px auto'}} key={arrays.id} data-aos="fade-up" className='product-card'>
+          <div style={{margin: '20px auto'}} key={arrays.id} className='product-card'>
             <img src={arrays.url} alt='clothes image number 1' />
             <h4 style={{ textAlign: 'center' }}>{arrays.title}</h4><br />
             <p>{arrays.description}</p><br />
